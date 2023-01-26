48f6dbd84df83712bba2360e127eea27b5c417e1
Vincent Guyot <guyot.vincent18@gmail.com>
2023-01-26 19:33:37 +0100

    map selection

---
+++ Project/Assets/Scripts/Menus/LevelSelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void LoadLevelPassed(string levelName) {
+        SceneManager.LoadScene(levelName);
+        Debug.Log("click");
+    }
+}
--- Project/Assets/Scripts/Menus/MainMenu.cs
+++ Project/Assets/Scripts/Menus/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public void PressSolo() {
-        SceneManager.LoadScene("SoloMapSelection");
+        SceneManager.LoadScene("MapSelection");
     }
 
     public void PressMultiplayer() {
